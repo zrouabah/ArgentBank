7bc4fe87b64c6d3dfe63dbdc38c3b80e963cb792
zrouabah <136023459+zrouabah@users.noreply.github.com>
2024-04-04 10:55:03 +0200

    Update SignIn.jsx

--- front/src/pages/SignIn.jsx
+++ front/src/pages/SignIn.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import Form from "../components/Form";
 
 function SignIn() {
+  const navigate = useNavigate();
+  const isLogged = useSelector(state => state.user.isLogged);
+
+  useEffect(() => {
+    if (isLogged) {
+      navigate('/');
+    }
+  }, []);
+
     return (
         <main className="main bg-dark">
             <section className="sign-in-content">
