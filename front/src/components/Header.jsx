b3563a4605bd9342097ab05b753cffe3a5308ff6
zrouabah <136023459+zrouabah@users.noreply.github.com>
2024-04-05 10:00:38 +0200

    Update Header.jsx
    Récupérer l'utilisateur si le token est présent mais que l'utilisateur n'est pas chargé

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,27 +1,45 @@
+
+import { useEffect } from "react";
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../redux/actions/user.actions';
+import { isLogged } from '../redux/actions/user.actions';
 
 import Logo from '../assets/images/argentBankLogo.webp';
 
 function Header() {
+    // Enregistrement du token avec useSelector
     const token = useSelector(state => state.user.token);
-    const user = useSelector(store => store.user);
+    const user = useSelector(store => store.user)
     const dispatch = useDispatch();
 
-    // Fonction de déconnexion
-    const handleLogout = () => {
-        dispatch(logout());
-    };
-
-    // Écouteur d'événement pour la navigation du navigateur
-    window.onpopstate = () => {
-        // Vérifiez si l'utilisateur est connecté et déconnectez-le
-        if (token) {
-            handleLogout();
-        }
-    };
-
+    // @TODO : Récupérer l'utilisateur si le token est présent mais que l'utilisateur n'est pas chargé. Le faire ici plutôt que sur la page User.
+    useEffect(() => {
+      if (token && !user.isLoaded) {
+          const fetchUser = async () => {
+              try {
+                  const response = await fetch('http://localhost:3001/api/v1/user/profile', {
+                      method: 'POST',
+                      headers: {
+                          'Content-Type': 'application/json',
+                          'Accept': 'application/json',
+                          'Authorization': `Bearer ${token}`
+                      },
+                      body: JSON.stringify({})
+                  });
+                  if (response.ok) {
+                      const data = await response.json();
+                      dispatch(isLogged(data.body));
+                  } else {
+                      throw new Error('Failed to fetch user profile');
+                  }
+              } catch (error) {
+                  console.error('Error fetching user profile:', error);
+              }
+          };
+          fetchUser();
+      }
+  }, [dispatch, token, user.isLoaded]);
     return (
         <header>
             <nav className="main-nav">
@@ -32,27 +50,30 @@ function Header() {
                 <div>
                     {
                         token ?
+                        // Si le token est là mettre le lien de déconnexion
                         <div className='user-nav'>
                             <NavLink className="user-nav-item" to={'/user'}>
                                 <p className='user-nav-name'>{user.userName}</p>
                                 <i className="user-nav-icon fa fa-user-circle"></i>
                             </NavLink>
-                                                    
-                            <NavLink className="user-nav-item" to={'/'} onClick={handleLogout}>
+
+                            <NavLink className="user-nav-item" to={'/'} onClick={() => dispatch(logout())}>
                                 <i className="user-nav-icon fa fa-sign-out"></i>
                                 Sign out
+
                             </NavLink>
                         </div>
                         :
+                        // Sinon laisser le Sign In
                         <NavLink className="main-nav-item" to={'/login'}>
                             <i className="fa fa-user-circle"></i>
                             Sign In
                         </NavLink>
                     }
-                </div>   
+                </div>
             </nav>
         </header>
-    );
+    )
 }
 
 export default Header;
