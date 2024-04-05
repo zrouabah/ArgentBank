1044027f2dcecad50d5eec61a7a1a6a0cc889de3
zrouabah <136023459+zrouabah@users.noreply.github.com>
2024-04-05 08:51:52 +0200

    Update Header.jsx
    modifs sur la connexion

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,61 +1,58 @@
-import React, { useEffect } from "react";
-import { NavLink, useLocation } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { logout } from "../redux/actions/user.actions";
+import { NavLink } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../redux/actions/user.actions';
 
-import Logo from "../assets/images/argentBankLogo.webp";
+import Logo from '../assets/images/argentBankLogo.webp';
 
 function Header() {
-  const token = useSelector((state) => state.user.token);
-  const user = useSelector((store) => store.user);
-  const dispatch = useDispatch();
-  const location = useLocation();
+    const token = useSelector(state => state.user.token);
+    const user = useSelector(store => store.user);
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    // Si l'utilisateur est connecté et se trouve sur la page d'accueil,
-    // déconnectez-le automatiquement
-    if (token && location.pathname === "/") {
-      dispatch(logout());
-    }
-  }, [dispatch, token, location.pathname]);
+    // Fonction de déconnexion
+    const handleLogout = () => {
+        dispatch(logout());
+    };
 
-  return (
-    <header>
-      <nav className="main-nav">
-        <NavLink className="main-nav-logo" to={"/"}>
-          <img
-            className="main-nav-logo-image"
-            src={Logo}
-            alt="Argent Bank Logo"
-          />
-          <h1 className="sr-only">Argent Bank</h1>
-        </NavLink>
-        <div>
-          {token ? (
-            <div className="user-nav">
-              <NavLink className="user-nav-item" to={"/user"}>
-                <p className="user-nav-name">{user.userName}</p>
-                <i className="user-nav-icon fa fa-user-circle"></i>
-              </NavLink>
-              <NavLink
-                className="user-nav-item"
-                to={"/"}
-                onClick={() => dispatch(logout())}
-              >
-                <i className="user-nav-icon fa fa-sign-out"></i>
-                Sign out
-              </NavLink>
-            </div>
-          ) : (
-            <NavLink className="main-nav-item" to={"/login"}>
-              <i className="fa fa-user-circle"></i>
-              Sign In
-            </NavLink>
-          )}
-        </div>
-      </nav>
-    </header>
-  );
+    // Écouteur d'événement pour la navigation du navigateur
+    window.onpopstate = () => {
+        // Vérifiez si l'utilisateur est connecté et déconnectez-le
+        if (token) {
+            handleLogout();
+        }
+    };
+
+    return (
+        <header>
+            <nav className="main-nav">
+                <NavLink className="main-nav-logo" to={'/'}>
+                    <img className="main-nav-logo-image" src={Logo} alt="Argent Bank Logo" />
+                    <h1 className="sr-only">Argent Bank</h1>
+                </NavLink>
+                <div>
+                    {
+                        token ?
+                        <div className='user-nav'>
+                            <NavLink className="user-nav-item" to={'/user'}>
+                                <p className='user-nav-name'>{user.userName}</p>
+                                <i className="user-nav-icon fa fa-user-circle"></i>
+                            </NavLink>
+                                                    
+                            <NavLink className="user-nav-item" to={'/'} onClick={handleLogout}>
+                                <i className="user-nav-icon fa fa-sign-out"></i>
+                                Sign out
+                            </NavLink>
+                        </div>
+                        :
+                        <NavLink className="main-nav-item" to={'/login'}>
+                            <i className="fa fa-user-circle"></i>
+                            Sign In
+                        </NavLink>
+                    }
+                </div>   
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
